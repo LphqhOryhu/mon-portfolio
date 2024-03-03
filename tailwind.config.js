cb954841190e89adfa8a108a8e2683e9c0dd7c4c
Hugo <hugo.de-brabander@campus-la-chataigneraie.org>
2024-03-03 19:11:35 +0100

    added tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  purge: [
+    './src/**/*.html',
+    './src/**/*.ts',
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
