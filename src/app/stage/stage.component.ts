d5f926ccf03c1d4a3f787c648d97369ec8284972
Hugo <hugo.de-brabander@campus-la-chataigneraie.org>
2024-03-06 06:29:19 +0100

    added logique stage

--- src/app/stage/stage.component.ts
+++ src/app/stage/stage.component.ts
@@ -3,10 +3,51 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-stage',
   standalone: true,
-  imports: [],
   templateUrl: './stage.component.html',
-  styleUrl: './stage.component.scss'
+  styleUrls: ['./stage.component.scss']
 })
 export class StageComponent {
+  showModalFlazio: boolean = false;
+  showModalSGA: boolean = false;
+  showMoreFlazio: boolean = false; 
+  showMoreSGA: boolean = false; 
 
+  selectOption(option: string) {
+    if (option === 'Flazio') {
+      this.openModalFlazio();
+    } else if (option === 'SGA') {
+      this.openModalSGA();
+    }
+  }
+
+  openModalFlazio() {
+    this.resetModals();
+    this.showModalFlazio = true;
+  }
+
+  openModalSGA() {
+    this.resetModals();
+    this.showModalSGA = true;
+  }
+
+  closeModals() {
+    this.resetModals();
+  }
+
+  viewMoreFlazio() {
+    this.resetModals();
+    this.showMoreFlazio = true;
+  }
+
+  viewMoreSGA() {
+    this.resetModals();
+    this.showMoreSGA = true;
+  }
+
+  private resetModals() {
+    this.showModalFlazio = false;
+    this.showModalSGA = false;
+    this.showMoreFlazio = false;
+    this.showMoreSGA = false;
+  }
 }
