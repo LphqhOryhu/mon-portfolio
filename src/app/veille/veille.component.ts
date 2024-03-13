a61bcd181b11c10c5397e0ad9178a5cad5dc4d39
Hugo <hugo.de-brabander@campus-la-chataigneraie.org>
2024-03-13 20:55:53 +0100

    added filter

--- src/app/veille/veille.component.ts
+++ src/app/veille/veille.component.ts
@@ -1,17 +1,26 @@
 import { Component } from '@angular/core';
 import { Veille } from '../models/veille.model';
 import { VeilleService } from '../veille.service';
+import {MatButtonToggleChange, MatButtonToggleModule} from '@angular/material/button-toggle';
 
 @Component({
   selector: 'app-veille',
   standalone: true,
-  imports: [],
+  imports: [
+    MatButtonToggleModule,
+
+  ],
   templateUrl: './veille.component.html',
   styleUrl: './veille.component.scss'
 })
 export class VeilleComponent {
   veilles: Veille[];
   showModalOutils: boolean = false;
+  selectedBornage: string = 'Méthodologies et Techniques de Pentest'; // valeur par défaut
+
+  onValueChange(event: MatButtonToggleChange) {
+    this.selectedBornage = event.value;
+  }
 
 
   constructor(private veilleService: VeilleService) { 
@@ -20,5 +29,9 @@ export class VeilleComponent {
 
   toggleModal() {
     this.showModalOutils = !this.showModalOutils;
+  }
+
+  updateSelectedBornage(bornage: string) {
+    this.selectedBornage = bornage;
   }
 }
